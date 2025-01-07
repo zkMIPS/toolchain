152c0acb3bf0075cd39e24d693a4c0391eb1e30f
qethu <190734095+qethu@users.noreply.github.com>
2025-01-07 18:30:22 +0800

    fix revme test

--- check.sh
+++ check.sh
@@ -7,5 +7,5 @@ cd zkm/prover/examples/sha2-rust/host
 ARGS="711e9609339e92b03ddc0a211827dba421f38f9ed8b9d806e1ffdd8c15ffa03d world!" RUST_LOG=info SEG_OUTPUT=/tmp/output cargo run --release
 
 cd $CWD
-cd prover/examples/revme/host
+cd zkm/prover/examples/revme/host
 RUST_LOG=info JSON_PATH=../../../../emulator/test-vectors/test.json SEG_OUTPUT=/tmp/output SEG_SIZE=262144 cargo run --release

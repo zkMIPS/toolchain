dede8f1b1c8330034d5d850ba6166a13ddc1cbc5
qethu <190734095+qethu@users.noreply.github.com>
2025-01-08 21:06:45 +0800

    Support mips-mti

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ TARGET=`rustc -vV | sed -n 's|host: ||p'`
 
 rm -rf $CWD/rust-staged
 
-cd $CWD/rust-workspace
+cd $CWD/rust
 ./x build library
 ./x build --stage 2 compiler/rustc
-BOOTSTRAP_SKIP_TARGET_SANITY=1 ./x build --target ${TARGET},mips-zkm-zkvm-elf
-BOOTSTRAP_SKIP_TARGET_SANITY=1 ./x install --target ${TARGET},mips-zkm-zkvm-elf
+BOOTSTRAP_SKIP_TARGET_SANITY=1 ./x build --target ${TARGET},mips-mti-none-elf,mipsel-mti-none-elf
+BOOTSTRAP_SKIP_TARGET_SANITY=1 ./x install --target ${TARGET},mips-mti-none-elf,mipsel-mti-none-elf
 
 
 cd $CWD/cargo
--- clone.sh
+++ clone.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-if [ -d rust-workspace ];then
-	git -C rust-workspace checkout .
-	git -C rust-workspace pull --rebase
+CWD=`pwd`
+
+if [ -d rust ];then
+	git -C rust checkout .
+	git -C rust pull --rebase
 else
-	git clone https://github.com/zkMIPS/rust-workspace.git
+	git clone https://github.com/rust-lang/rust.git
 fi
-git -C rust-workspace checkout Triple_mips-zkm-zkvm-elf
-sed -i 's@^cc =.*@cc = { git = "https://github.com/zkMIPS/cc-rs.git", branch = "Triple_mips-zkm-zkvm-elf" }@' rust-workspace/src/bootstrap/Cargo.toml
-cp -f rust-workspace/config.example.toml rust-workspace/config.toml
-sed -i 's@#prefix = .*@prefix = "/var/jenkins_home/workspace/rust-toolchain/rust-staged"@' rust-workspace/config.toml
-sed -i 's@#sysconfdir = .*@sysconfdir = "etc"@' rust-workspace/config.toml
-sed -i 's@#docs = .*@docs = false@' rust-workspace/config.toml
-sed -i 's@#lld = .*@lld = true@' rust-workspace/config.toml
-sed -i 's@#download-ci-llvm = .*@download-ci-llvm = false@' rust-workspace/config.toml
+
+git -C rust checkout master
+sed -i 's@^cc =.*@cc = { git = "https://github.com/zkMIPS/cc-rs.git", branch = "mips-mti" }@' rust/src/bootstrap/Cargo.toml
+cp -f rust/config.example.toml rust/config.toml
+sed -i "s@#prefix = .*@prefix = \"${CWD}/rust-staged\"@" rust/config.toml
+sed -i 's@#sysconfdir = .*@sysconfdir = "etc"@' rust/config.toml
+sed -i 's@#docs = .*@docs = false@' rust/config.toml
+sed -i 's@#lld = .*@lld = true@' rust/config.toml
+sed -i 's@#download-ci-llvm = .*@download-ci-llvm = false@' rust/config.toml
 
 
 if [ -d cargo ];then
@@ -29,3 +32,9 @@ if [ -d zkm ];then
 else
 	git clone https://github.com/zkMIPS/zkm.git
 fi
+for i in `grep -r mips-zkm zkm/prover zkm/build | awk -F':' '{print $1}' | sort | uniq`;do
+	sed -i 's/mips-zkm-zkvm-elf/mips-mti-none-elf/g' $i
+done
+for i in `grep -r 'target_os = "zkvm"' zkm/runtime | awk -F':' '{print $1}' | sort | uniq`;do
+	sed -i 's/target_os = "zkvm"/target_os = "none"/g' $i
+done

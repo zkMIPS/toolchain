75abcc7d75ab0874643105e3a09f47dc5c5afcb5
qethu <190734095+qethu@users.noreply.github.com>
2025-06-09 16:57:21 +0800

    check.sh: run cargo clean

--- check.sh
+++ check.sh
@@ -8,31 +8,38 @@ rustc --version
 ## FAIL due to ring 0.16.20 too old
 #cd $CWD
 #cd zkm/prover/examples/keccak
+#cargo clean
 #RUST_LOG=info SEG_FILE_DIR=/tmp/output cargo run --release
 
 ## FAIL due to ring 0.16.20 too old
 #cd $CWD
 #cd zkm/prover/examples/prove-large-seg
+#cargo clean
 #RUST_LOG=info SEG_FILE_DIR=/tmp/output cargo run --release
 
 cd $CWD
 cd zkm/prover/examples/prove-seg
+cargo clean
 RUST_LOG=info SEG_FILE_DIR=/tmp/output cargo run --release
 
 cd $CWD
 cd zkm/prover/examples/revme/host
+cargo clean
 RUST_LOG=info SEG_OUTPUT=/tmp/output cargo run --release
 
 cd $CWD
 cd zkm/prover/examples/sha2-rust/host
+cargo clean
 ARGS="711e9609339e92b03ddc0a211827dba421f38f9ed8b9d806e1ffdd8c15ffa03d world!" RUST_LOG=info SEG_OUTPUT=/tmp/output cargo run --release
 
 cd $CWD
 cd zkm/prover/examples/sha2-composition/host
+cargo clean
 RUST_LOG=info PRECOMPILE_PATH=../../sha2-rust/guest/elf/mips-zkm-zkvm-elf SEG_OUTPUT=/tmp/output cargo run --release
 
 cd $CWD
 cd zkm/prover/examples/split-seg
+cargo clean
 BASEDIR=../../../emulator/test-vectors RUST_LOG=info ELF_PATH=../../../emulator/test-vectors/minigeth BLOCK_NO=13284491 SEG_OUTPUT=/tmp/output SEG_SIZE=65536 ARGS="" cargo run --release
 BASEDIR=../../../emulator/test-vectors RUST_LOG=info ELF_PATH=../../../emulator/test-vectors/minigeth BLOCK_NO=13284491 SEG_OUTPUT=/tmp/output SEG_START_ID=0 SEG_NUM=1 SEG_SIZE=65536 cargo run --release
 BASEDIR=../../../emulator/test-vectors RUST_LOG=info ELF_PATH=../../../emulator/test-vectors/minigeth BLOCK_NO=13284491 SEG_OUTPUT=/tmp/output SEG_START_ID=0 SEG_NUM=299 SEG_SIZE=65536 cargo run --release
@@ -41,68 +48,85 @@ BASEDIR=../../../emulator/test-vectors RUST_LOG=info ELF_PATH=../../../emulator/
 
 cd $CWD
 cd zkMIPS/examples/aggregation/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/bn254/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/chess/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/cycle-tracking/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/fibonacci/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 #cd zkMIPS/examples/fibonacci_c_lib/host
+#cargo clean
 #RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/groth16/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/groth16/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/is-prime/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/json/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD # FAILED: assertion `left == right` failed
 #cd zkMIPS/examples/keccak-precompile/host
+#cargo clean
 #RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/plonk/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/regex/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/rsa/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/ssz-withdrawals/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/tendermint/host
+cargo clean
 RUST_LOG=info cargo run --release
 
 cd $CWD
 cd zkMIPS/examples/unconstrained/host
+cargo clean
 RUST_LOG=info cargo run --release

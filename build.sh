657403dc6f50929617a6d4489b56cf1702762239
qethu <190734095+qethu@users.noreply.github.com>
2025-07-03 17:41:09 +0800

    build clippy

--- build.sh
+++ build.sh
@@ -14,15 +14,28 @@ done
 ./x build --stage 2 compiler/rustc
 BOOTSTRAP_SKIP_TARGET_SANITY=1 ./x build --target ${TARGET},mips-zkm-zkvm-elf,mipsel-zkm-zkvm-elf
 BOOTSTRAP_SKIP_TARGET_SANITY=1 ./x install --target ${TARGET},mips-zkm-zkvm-elf,mipsel-zkm-zkvm-elf
+cp -rf ./build/${TARGET}/stage1/lib/rustlib/${TARGET}/lib/* $CWD/rust-staged/lib/rustlib/${TARGET}/lib
 
 
 cd $CWD/cargo
 cargo --version
 cargo clean
+git clean -fdx
 cargo build --release
 cargo install --path . --root=$CWD/rust-staged
 
+cd $CWD/rust-clippy
+cargo clean
+git clean -fdx
+oldPATH=$PATH
+export PATH=$CWD/rust-staged/bin:$PATH
+cargo build --release
+cargo install --path . --root=$CWD/rust-staged
+export PATH=$oldPATH
+
 patchelf --set-rpath '$ORIGIN/../lib' $CWD/rust-staged/bin/cargo
+patchelf --set-rpath '$ORIGIN/../lib' $CWD/rust-staged/bin/cargo-clippy
+patchelf --set-rpath '$ORIGIN/../lib' $CWD/rust-staged/bin/clippy-driver
 cp -f /usr/lib64/libcrypto.so.3* /usr/lib64/libssl.so.3* $CWD/rust-staged/lib
 
 cd $CWD
@@ -38,3 +51,9 @@ echo >> $CWD/rust-staged/buildinfo
 echo "Cargo:" >> $CWD/rust-staged/buildinfo
 git -C cargo log -1 >> $CWD/rust-staged/buildinfo
 echo >> $CWD/rust-staged/buildinfo
+
+echo "Clippy:" >> $CWD/rust-staged/buildinfo
+git -C rust-clippy log -1 >> $CWD/rust-staged/buildinfo
+echo >> $CWD/rust-staged/buildinfo
+
+cat $CWD/rust-staged/buildinfo
